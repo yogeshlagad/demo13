bb30c9728df7889123e490652d39767b9e112dc4
yogeshlagad <101618435+yogeshlagad@users.noreply.github.com>
2022-03-16 08:42:15 +0530

    emp.gretter

---
+++ Employee.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Employee {
+	public static void main(String[] args) {
+		String name;
+		int age;
+		float salary;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Name = ");
+		name = sc.nextLine();
+		System.out.println("Age = ");
+		age = sc.nextInt();
+		System.out.println("Salary = ");
+		salary = sc.nextFloat();
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(salary);
+
+	}
+
+}
---
+++ Gretter.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Gretter {
+
+	public static void main(String[] args) {
+		int num1, num2;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Num1");
+		num1 = sc.nextInt();
+		System.out.println("Enter Num2");
+		num2 = sc.nextInt();
+      
+         if(num1>num2)
+         {
+        	 System.out.println("Num1 is gretter"); 
+         }
+         else
+         {
+        	 System.out.println("Num2 is gretter"); 
+         }
+	}
+
+}
